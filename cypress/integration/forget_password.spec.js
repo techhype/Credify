497ecac88adc802c039b3b579f1fa3f163bb99c4
techhype <krizzkishore@gmail.com>
2021-03-25 22:21:09 +0530

    Modied e2e forgot pw

--- cypress/integration/forget_password.spec.js
+++ cypress/integration/forget_password.spec.js
@@ -1,10 +1,10 @@
 describe('Forget password', function () {
     it('Changes new password and logs in with it ', function () {
-        const new_password = 'vasi'
+        const new_password = 'kishore'
         cy.visit('http://34.122.145.124')
         cy.contains('credify')
         cy.contains('Forgot Password').click()
-        cy.get('input[name="Email"]').type('vasanthl@virtusa.com')
+        cy.get('input[name="Email"]').type('akishore@virtusa.com')
         cy.get('input[name="Password"]').type(new_password)
         cy.get('input[name="ConfirmPassword"]').type(new_password)
         cy.contains('Submit').click()
@@ -12,7 +12,7 @@ describe('Forget password', function () {
         cy.wait(2000)
 
         //login check with new password
-        cy.get('input:first').type('vasanthl@virtusa.com')
+        cy.get('input:first').type('akishore@virtusa.com')
         cy.get('input:Password').type(new_password)
         cy.contains('Sign In').click()
     
